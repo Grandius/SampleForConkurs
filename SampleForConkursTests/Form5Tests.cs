0bc983f5f7907a589103389b3b9a89523fc148de
Grandius <gradius777@gmail.com>
2017-06-02 08:35:41 +0300

    Add files via upload

---
+++ SampleForConkursTests/Form5Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleForConkurs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleForConkurs.Tests
+{
+    [TestClass()]
+    public class Form5Tests
+    {
+        [TestMethod()]
+        public void ExportTableTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
